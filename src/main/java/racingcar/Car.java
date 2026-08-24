de6a64662a0a7896740f4d2d079134c2ea15cbae
johaeunn <jheun04@naver.com>
2026-08-24 20:39:40 +0900

    feat: Car 클래스 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import java.util.Random;
+
+public class Car {
+    private final static int MOVE_THRESHOLD = 4;
+    private String name;
+    private int location;
+
+    public Car() {
+        location = 0;
+    }
+
+    public void move() {
+        int number = randomGenerator();
+        if ( number >= MOVE_THRESHOLD ) {
+            location++;
+        }
+    }
+
+    private int randomGenerator() {
+        Random random = new Random();
+        return random.nextInt(10);
+    }
+}
