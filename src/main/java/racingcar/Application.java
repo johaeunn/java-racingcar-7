b9e3debc9c17f98602cc3bfa5c94f36154e9565c
johaeunn <jheun04@naver.com>
2026-08-24 21:34:13 +0900

    feat: Application 흐름 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,22 @@
 package racingcar;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String inputNames = scanner.nextLine();
+        List<String> names = List.of(inputNames.split(","));
+        Cars cars = new Cars(names);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int round = scanner.nextInt();
+
+        RacingGame racingGame = new RacingGame(cars, round);
+        racingGame.startingGame();
+
+
     }
 }
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGame {
-    List<String> names;
-    Cars cars = new Cars(names);
+    private int round;
+    private Cars cars;
+
+    public RacingGame(Cars cars, int round) {
+        this.round = round;
+        this.cars = cars;
+    }
 
     private int calculateMaxLocation() {
         int max = 0;
@@ -26,5 +31,24 @@ public class RacingGame {
             }
         }
         return winnerList;
+    }
+
+    public void startingGame() {
+        for (int i = 0; i < round; i++) {
+            moveCars(cars);
+        }
+    }
+
+    private void moveCars(Cars cars) {
+        for(Car car : cars.getCars()) {
+            car.move();
+            printResult(car);
+        }
+    }
+
+    private void printResult(Car car) {
+        int location = car.getLocation();
+        String dash = "-";
+        System.out.println(car.getName() + " : " + dash.repeat(location));
     }
 }
